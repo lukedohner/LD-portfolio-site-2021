45be571db29ea78b15903c41807459496e94d158
lukedohner <lukedohner@whoisstudio.com>
2021-08-26 17:34:18 -0400

    removed npm mod Sharp

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -10,10 +10,6 @@ var plugins = [{
       name: 'gatsby-plugin-image',
       plugin: require('/luke_projects_all/Luke-site-calypsoBootstrap/calypso/gatsby-ld/LukeSite-Build-From-Scratch-Aug11-2021/LD-portfolio-site/node_modules/gatsby-plugin-image/gatsby-ssr'),
       options: {"plugins":[]},
-    },{
-      name: 'gatsby-plugin-manifest',
-      plugin: require('/luke_projects_all/Luke-site-calypsoBootstrap/calypso/gatsby-ld/LukeSite-Build-From-Scratch-Aug11-2021/LD-portfolio-site/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
-      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/gatsby-icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"cde27a5802be429aafe6f7a2f637ac24"},
     }]
 /* global plugins */
 // During bootstrap, we write requires at top of this file which looks like:
